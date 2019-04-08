01211ed387177d83d3bfb35cae2f89dfd7e02596
VarnerDamasceno <47373500+VarnerDamasceno@users.noreply.github.com>
2019-04-08 14:26:52 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+<form class="" action="index.html" method="post">
+   <input type="text" name="usuario" placeholder="Nome do usuário" placeholder="Usuário">
+   <input type="password" name="senha" value="senha" placeholder="Senha">
+   <input type="submit" name="entrar" value="entrar" >
+</form>
+
+  </body>
+</html>
